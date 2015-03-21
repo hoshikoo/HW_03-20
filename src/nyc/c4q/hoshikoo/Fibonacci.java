946be268f0fe8e1a35175fd88e60ebb0bfc70813
hoshikoo <hoshikooki@gmail.com>
2015-03-21 13:31:44 -0400

    fix fibonacci.java

--- src/nyc/c4q/hoshikoo/Fibonacci.java
+++ src/nyc/c4q/hoshikoo/Fibonacci.java
@@ -3,23 +3,27 @@ package nyc.c4q.hoshikoo;
 /**
  * Created by Hoshiko on 3/16/15.
  */
+
+import java.text.DecimalFormat;
 public class Fibonacci {
 
     public static void main(String[] args) {
-        finonacci(80);
+        finonacci(110);
     }
 
     public static void finonacci(long N){
-        long F0 = 0, F1 = 1, Fn;
+        double F0 = 0, F1 = 1, Fn;
 
         // System.out.println(F0 + "\n" + F1);
 
-        for (long i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++) {
             Fn=F0+F1;
             //System.out.println(Fn);
             F0=F1;
             F1=Fn;
         }
-        System.out.println(F0);
+        DecimalFormat df = new DecimalFormat("###,##0.00");
+        System.out.println(df.format(F0));
+        //System.out.println(F0);
     }
 }
